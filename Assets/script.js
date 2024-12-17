b27c10cb06f34e83c98992db35971bfb22beb032
beanbean <gracefernandez00@icloud.com>
2024-12-16 21:21:50 -0700

    JS updates assisted

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,12 @@
-const recipeInput = document.querySelector('#recipe-text');
+// const recipeInput = document.querySelector('#recipe-text');
 const recipeForm = document.querySelector('#recipe-form');
 const recipeList = document.querySelector('#recipe-list');
 
-let recipeName = document.querySelector('recipe-name');
+let recipeName = document.querySelector('#recipe-name');
 let recipeInstructions = document.querySelector('#recipe-instructions');
 let recipeIngredients = document.querySelector('#recipe-ingredients');
-let recipeCategory = document.getElementById('#category-dropdown');
-let recipeServings = document.querySelector('recipe-servings');
+let recipeCategory = document.getElementById('category-dropdown');
+let recipeServings = document.querySelector('#recipe-servings');
 
 let recipes = [];
 
@@ -17,8 +17,16 @@ function renderRecipes() {
         const recipe = recipes[i];
 
         const li = document.createElement('li');
-        li.textContent = recipe;
         li.setAttribute('data-index', i);
+
+        const h2 = document.createElement('h2');
+        h2.textContent = recipe.recipeName;
+
+        const h3 = document.createElement('h3');
+        h3.textContent = recipe.recipeInstructions;
+
+        li.appendChild(h2)
+        li.appendChild(h3)
 
         const button = document.createElement('button');
         button.textContent = 'Delete Recipe';
@@ -53,31 +61,42 @@ function removeRecipe(index) {
 recipeForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const recipeText = recipeInput.value.trim();
-
-    if (recipeText === '') {
-        return;
-    }
+    const recipeNameValue = recipeName.value.trim();
+    const recipeInstructionsValue = recipeInstructions.value.trim();
 
 
-    recipes.push(recipeText);
-    recipeInput.value = '';
+    const recipeData = {
+        recipeName: recipeNameValue,
+        recipeInstructions: recipeInstructionsValue
+    };
+
+    console.log(recipeData)
+
+
+
+    // if (recipeText === '') {
+    //     return;
+    // }
+
+
+    recipes.push(recipeData);
+    // recipeInput.value = '';
 
     storeRecipes();
     renderRecipes();
 });
 
-recipeList.addEventListener('click', function (event) {
-    const element = event.target;
+// recipeList.addEventListener('click', function (event) {
+//     const element = event.target;
 
-    if (element.matches('button') === true) {
+//     if (element.matches('button') === true) {
 
-        const index = element.parentElement.getAttribute('data-index');
-        recipes.splice(index, 1);
+//         const index = element.parentElement.getAttribute('data-index');
+//         recipes.splice(index, 1);
 
-        storeRecipes();
-        renderRecipes();
-    }
-});
+//         storeRecipes();
+//         renderRecipes();
+//     }
+// });
 
 init();
